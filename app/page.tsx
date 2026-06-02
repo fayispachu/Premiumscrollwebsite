49239652bf5ede192e172b6487a6fc1f04695bc5
Fayis <fayizpachu217@gmail.com>
2026-06-02 22:46:40 +0530

    12

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ import { motion } from 'framer-motion';
 import gsap from 'gsap';
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
 import Lenis from 'lenis';
-
+// a
 gsap.registerPlugin(ScrollTrigger);
 
 const services = [
